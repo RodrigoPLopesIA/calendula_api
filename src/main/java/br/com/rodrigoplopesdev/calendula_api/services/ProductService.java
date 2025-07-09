a62ca88d9bf563b1bba142c0b6a90da45dd13f5a
RodrigoPLopesIA <rodrigoplopes25@gmail.com>
2025-07-09 20:31:09 -0300

    feat:  implement repository on service and solving connection bugs

--- src/main/java/br/com/rodrigoplopesdev/calendula_api/services/ProductService.java
+++ src/main/java/br/com/rodrigoplopesdev/calendula_api/services/ProductService.java
@@ -1,17 +1,21 @@
 package br.com.rodrigoplopesdev.calendula_api.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.rodrigoplopesdev.calendula_api.dtos.CreateProductDTO;
 import br.com.rodrigoplopesdev.calendula_api.models.Product;
 import br.com.rodrigoplopesdev.calendula_api.patterns.factory.ProductFactory;
+import br.com.rodrigoplopesdev.calendula_api.repositories.ProductRepository;
 
 @Service
 public class ProductService {
 
+    @Autowired
+    private ProductRepository productRepository;
     public Product save(CreateProductDTO product) {
-
-        return ProductFactory.getInstance(product);
+        Product instance = ProductFactory.getInstance(product);;
+        return this.productRepository.save(instance);
     }
     
 
