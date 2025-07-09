ddeca6f03ef41a9251eabb61ce2801f0ef719d46
RodrigoPLopesIA <rodrigoplopes25@gmail.com>
2025-07-09 20:13:51 -0300

    feat: create product repository and remove unused imports

--- src/main/java/br/com/rodrigoplopesdev/calendula_api/dtos/CreateProductDTO.java
+++ src/main/java/br/com/rodrigoplopesdev/calendula_api/dtos/CreateProductDTO.java
@@ -3,7 +3,6 @@ package br.com.rodrigoplopesdev.calendula_api.dtos;
 import lombok.Builder;
 
 import java.util.List;
-import java.util.UUID;
 
 @Builder
 public record CreateProductDTO(String title, String description, List<String> colors, Double price) {
---
+++ src/test/java/br/com/rodrigoplopesdev/calendula_api/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package br.com.rodrigoplopesdev.calendula_api.repositories;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import br.com.rodrigoplopesdev.calendula_api.models.Product;
+
+public interface ProductRepository extends MongoRepository<Product, String> {
+    
+}
