ae2034657b973919675eede23a7dc34daef5c50d
RodrigoPLopesIA <rodrigoplopes25@gmail.com>
2025-07-09 18:29:33 -0300

    feat: implement product service in product controller

--- src/main/java/br/com/rodrigoplopesdev/calendula_api/controllers/ProductController.java
+++ src/main/java/br/com/rodrigoplopesdev/calendula_api/controllers/ProductController.java
@@ -2,9 +2,13 @@ package br.com.rodrigoplopesdev.calendula_api.controllers;
 
 import br.com.rodrigoplopesdev.calendula_api.dtos.CreateProductDTO;
 import br.com.rodrigoplopesdev.calendula_api.dtos.ProductDTO;
+import br.com.rodrigoplopesdev.calendula_api.models.Product;
+import br.com.rodrigoplopesdev.calendula_api.services.ProductService;
+
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -15,10 +19,13 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/api/v1/products")
 public class ProductController {
+
+    @Autowired
+    private ProductService productService;
     @PostMapping
     public ResponseEntity create(@RequestBody CreateProductDTO data) {
-        ProductDTO productDTO = new ProductDTO(UUID.fromString("722dc867-ea3d-40a5-936d-70e29e830b99"), "Test", "test", 25.50, "15cm", LocalDate.now(), LocalDate.now());
-        return ResponseEntity.ok().body(productDTO);
+        Product product = this.productService.save(data);
+        return ResponseEntity.ok().body(product);
     }
     
 }
