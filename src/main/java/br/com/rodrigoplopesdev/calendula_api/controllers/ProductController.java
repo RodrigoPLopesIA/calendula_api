4b897876eae021a0bcb31e07bb92b16480957e31
RodrigoPLopesIA <rodrigoplopes25@gmail.com>
2025-07-09 20:06:05 -0300

    feat: create docker compose database

--- src/main/java/br/com/rodrigoplopesdev/calendula_api/controllers/ProductController.java
+++ src/main/java/br/com/rodrigoplopesdev/calendula_api/controllers/ProductController.java
@@ -14,8 +14,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.net.URI;
-import java.time.LocalDate;
-import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/products")
