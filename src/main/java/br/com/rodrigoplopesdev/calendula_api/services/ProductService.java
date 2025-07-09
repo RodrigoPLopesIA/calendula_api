7a007799104a571b49571148a1a3a861039f5102
RodrigoPLopesIA <rodrigoplopes25@gmail.com>
2025-07-09 17:57:10 -0300

    feat: create test for save new product

---
+++ src/main/java/br/com/rodrigoplopesdev/calendula_api/services/ProductService.java
@@ -0,0 +1,15 @@
+package br.com.rodrigoplopesdev.calendula_api.services;
+
+import br.com.rodrigoplopesdev.calendula_api.dtos.CreateProductDTO;
+
+public class ProductService {
+
+    public Object save(CreateProductDTO any) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'save'");
+    }
+    
+
+
+    
+}
--- src/test/java/br/com/rodrigoplopesdev/calendula_api/controllers/ProductControllerTest.java
+++ src/test/java/br/com/rodrigoplopesdev/calendula_api/controllers/ProductControllerTest.java
@@ -1,12 +1,19 @@
 package br.com.rodrigoplopesdev.calendula_api.controllers;
 
 import br.com.rodrigoplopesdev.calendula_api.dtos.CreateProductDTO;
+import br.com.rodrigoplopesdev.calendula_api.dtos.ProductDTO;
+import br.com.rodrigoplopesdev.calendula_api.services.ProductService;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
@@ -21,12 +28,17 @@ public class ProductControllerTest {
     @Autowired
     private MockMvc mvc;
 
+    @Mock
+    private ProductService productService;
+
     @Test
     @DisplayName("POST /api/v1/products")
     public void shouldSaveProduct() throws Exception{
 
         CreateProductDTO data = new CreateProductDTO("Test", "test", 25.05, "15cm");
         String json = new ObjectMapper().writeValueAsString(data);
+
+        BDDMockito.given(productService.save(Mockito.any(CreateProductDTO.class))).willReturn(ProductDTO.class);
 
         MockHttpServletRequestBuilder request = MockMvcRequestBuilders
                 .post("/api/v1/products")
