b9d019562161ed0db55fd3dc1f31be280df6cd34
RodrigoPLopesIA <rodrigoplopes25@gmail.com>
2025-07-22 11:15:21 -0300

    feat: enable mongo auditing confg to create fields createdAt and updatedAt

--- src/main/java/br/com/rodrigoplopesdev/calendula_api/models/Product.java
+++ src/main/java/br/com/rodrigoplopesdev/calendula_api/models/Product.java
@@ -1,10 +1,14 @@
 package br.com.rodrigoplopesdev.calendula_api.models;
 
+import java.time.Instant;
 import java.util.List;
-import java.util.UUID;
 
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.config.EnableReactiveMongoAuditing;
 import org.springframework.data.mongodb.core.mapping.Document;
+
 
 import lombok.Builder;
 import lombok.Data;
@@ -20,9 +24,19 @@ public class Product {
 
     @Id
     private String id;
+    
     private String title;
     private String description;
+    
     private Double price;
     private List<String> colors;
+
+    private List<String> images;
+
+    @CreatedDate
+    private Instant createdAt;
+
+    @LastModifiedDate
+    private Instant updatedAt;
 
 }
