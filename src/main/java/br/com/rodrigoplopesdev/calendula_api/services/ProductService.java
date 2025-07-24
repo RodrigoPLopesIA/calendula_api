1b53d48f6bd8cbd91abc852cdb659f2eedf27c83
RodrigoPLopesIA <rodrigoplopes25@gmail.com>
2025-07-24 19:24:51 -0300

    refactor: change method to return page of products

--- src/main/java/br/com/rodrigoplopesdev/calendula_api/services/ProductService.java
+++ src/main/java/br/com/rodrigoplopesdev/calendula_api/services/ProductService.java
@@ -3,6 +3,8 @@ package br.com.rodrigoplopesdev.calendula_api.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import br.com.rodrigoplopesdev.calendula_api.dtos.CreateProductDTO;
@@ -61,8 +63,8 @@ public class ProductService {
         this.productRepository.delete(product);
     }
 
-    public List<Product> findAll() {
-        return productRepository.findAll();
+    public Page<Product> findAll(Pageable page) {
+        return productRepository.findAll(page);
     }
 
 }
