d5a85f4be28f7d6b811c3fa3cbb099a3826fee37
RodrigoPLopesIA <rodrigoplopes25@gmail.com>
2025-07-27 23:06:22 -0300

    feat: implement user details interface in user model

--- src/main/java/br/com/rodrigoplopesdev/calendula_api/models/User.java
+++ src/main/java/br/com/rodrigoplopesdev/calendula_api/models/User.java
@@ -8,8 +8,12 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.Instant;
+import java.util.Collection;
+import java.util.List;
 
 @Builder
 @Getter
@@ -19,7 +23,7 @@ import java.time.Instant;
 @EqualsAndHashCode(of = "id")
 @ToString
 @Document(collection = "users")
-public class User {
+public class User implements UserDetails {
 
     @Id
     private String id;
@@ -65,5 +69,35 @@ public class User {
         this.setLastName(lastName);
         this.setEmail(email);
         this.setPassword(password);
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getUsername() {
+        return this.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
